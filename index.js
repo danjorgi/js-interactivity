12d2990821d3794de8d24a6df2be02b1709e84cf
Daniel Jorgensen <dan.r.jorgensen@gmail.com>
2023-10-09 13:41:47 -0600

    Step 3 Complete

--- index.js
+++ index.js
@@ -8,11 +8,19 @@ const movieTitle = document.createElement("span");
 movieTitle.textContent = inputField.value;
 movie.appendChild(movieTitle);
 
+const deleteBtn = document.createElement("button");
+deleteBtn.textContent = "X";
+deleteBtn.addEventListener("click", deleteMovie);
+movie.appendChild(deleteBtn);
+
 const list = document.querySelector("ul");
 list.appendChild(movie);
 
-inputField.value = ''
+inputField.value = '';
 }
 
 document.querySelector("form").addEventListener("submit", addMovie);
 
+let deleteMovie = (event) => {
+    event.target.parentNode.remove();
+}

aa0463247939d6d1b98ae97494b91c61f3631482
Daniel Jorgensen <dan.r.jorgensen@gmail.com>
2023-10-09 13:45:55 -0600

    Step 4 Complete

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const movie = document.createElement('li');
 
 const movieTitle = document.createElement("span");
 movieTitle.textContent = inputField.value;
+movieTitle.addEventListener("click", crossOffMovie);
 movie.appendChild(movieTitle);
 
 const deleteBtn = document.createElement("button");
@@ -23,4 +24,8 @@ document.querySelector("form").addEventListener("submit", addMovie);
 
 let deleteMovie = (event) => {
     event.target.parentNode.remove();
+}
+
+let crossOffMovie = (event) => {
+    event.target.classList.toggle('checked');
 }

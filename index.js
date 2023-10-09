818b08e15f1e8b8a0d644f9d0c81fee5bf5b5248
Daniel Jorgensen <dan.r.jorgensen@gmail.com>
2023-10-09 13:34:16 -0600

    Step 2 Complete

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('hello world')
+let addMovie = (event) => {
+event.preventDefault();
+let inputField = document.querySelector('input');
+
+const movie = document.createElement('li');
+
+const movieTitle = document.createElement("span");
+movieTitle.textContent = inputField.value;
+movie.appendChild(movieTitle);
+
+const list = document.querySelector("ul");
+list.appendChild(movie);
+
+inputField.value = ''
+}
+
+document.querySelector("form").addEventListener("submit", addMovie);
+
